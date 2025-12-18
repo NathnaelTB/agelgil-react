95ea4c4d5bc011b51e6d12fb00301031192918ff
NathnaelTB <natibekaffa12@gmail.com>
2025-12-18 09:35:23 +0300

    feat: Scroll-to-top feature when routing to another page added

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,12 @@ import NotFound from './pages/NotFound'
 import NewsDetail from './pages/NewsDetail'
 import NewsPage from './pages/NewsPage'
 import ProductCard from './components/ProductCard'
+import ScrollToTop from './components/ScrollToTop'
+
 const App = () => {
 
   const mapFrame = `<iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31268.897251533203!2d37.34540609664157!3d11.579636971800191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1644cffd70f1e1df%3A0xc1464dcdf79b85bd!2zbGVtbGVtaXR1IEJhaGlyZGFyIENvbW1lcmNpYWwgQ2VudGVyIOGIiOGIneGIiOGImuGJsSDhiaPhiIXhiK3hi7PhiK0g4Yuo4YyI4Ymg4YurIOGIm-GKpeGKqOGIjQ!5e0!3m2!1sen!2set!4v1692539134597!5m2!1sen!2set"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31268.897251533203!2d37.34540609664157!3d11.579636971800191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1644cffd70f1e1df%3A0xc1464dcdf79b85bd!2zbGVtbGVtaXR1IEJhaGlyZGFyIENvbW1lcmNpYWwgQ2VudGVyIOGIiOGIneGIiOGImuGJsSDhiaPhiIXhiK3hi7PhiK0g4Yuo4YyI4Ymg4YurIOGIm-GKleGKqOGIjQ!5e0!3m2!1sen!2set!4v1692539134597!5m2!1sen!2set"
           width="100%"
           height="100%"
           style="border:0;"
@@ -27,6 +29,7 @@ const App = () => {
   return (
     <>
       <main className="relative flex flex-col">
+        <ScrollToTop />
         <Nav />
         {/* Navbar  */}
 
